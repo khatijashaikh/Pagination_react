43c0b007ab94dc3fd20f65ef437106513f86831c
khatija <shaikhkhatijabi03@gmail.com>
2024-12-03 12:13:05 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ReactPaginate from 'react-paginate';
 
 function App() {
+  const handlePageClick=()=>{}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={20}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+   </div>
   );
 }
 
